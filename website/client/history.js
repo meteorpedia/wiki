507d7f11c2e4ab23064d304f61fe641f4b5d93ff
Bjorn Tipling <bjorn@ambientchill.com>
2013-06-22 09:54:39 -0700

    Add history edit nav.

--- website/client/history.js
+++ website/client/history.js
@@ -2,19 +2,32 @@
  * @fileOverview The wiki read controller.
  */
 
-var HP, SESSION_EDIT_USER_MAP, SESSION_EDIT_TS;
+var HP, SESSION_HISTORY_USER_MAP, SESSION_HISTORY_TS, SESSION_HISTORY_PREV,
+  SESSION_HISTORY_NEXT;
 
 /**
  * @type {string}
  * @const
  */
-SESSION_EDIT_USER_MAP = 'edit-user-map';
+SESSION_HISTORY_PREV = 'history-previous';
 
 /**
  * @type {string}
  * @const
  */
-SESSION_EDIT_TS = 'edit-timestamp';
+SESSION_HISTORY_NEXT = 'history-next';
+
+/**
+ * @type {string}
+ * @const
+ */
+SESSION_HISTORY_USER_MAP = 'history-user-map';
+
+/**
+ * @type {string}
+ * @const
+ */
+SESSION_HISTORY_TS = 'history-timestamp';
 
 /**
  * @constructor
@@ -56,22 +69,44 @@ HP.render = function(state, viewName, pageName, ts) {
   Session.set(SESSION_PAGE_NAME_KEY, pageName);
   Session.set(SESSION_PAGE_TYPE, viewName);
   if (_.isString(ts)) {
-    Session.set(SESSION_EDIT_TS, parseInt(ts, 10));
+    Session.set(SESSION_HISTORY_TS, parseInt(ts, 10));
   } else {
-    Session.set(SESSION_EDIT_TS, null);
+    Session.set(SESSION_HISTORY_TS, null);
   }
 };
 
 /** @inheritDoc */
 HP.dispose = function() {
-  Session.set(SESSION_EDIT_TS, null);
+  Session.set(SESSION_HISTORY_TS, null);
 };
 
 History = History_;
 
 Meteor.startup(function() {
-  Deps.autorun(createProfileFetcher(HP.name, SESSION_EDIT_USER_MAP,
+  // In startup because it uses createProfileFetcher, defined elsewhere.
+  Deps.autorun(createProfileFetcher(HP.name, SESSION_HISTORY_USER_MAP,
     'historyProfiles', WikiEdits));
+});
+
+Deps.autorun(function() {
+  var edits, prev, next, found, ts;
+  ts = Session.get(SESSION_HISTORY_TS);
+  edits = WikiEdits.find({pageId: pageId()}, {sort: {ts: 1}});
+  edits.forEach(function(edit) {
+    if (edit.ts === ts) {
+      found = true;
+      return;
+    }
+    if (!found) {
+      prev = edit;
+      return;
+    }
+    if (!next) {
+      next = edit;
+    }
+  });
+  Session.set(SESSION_HISTORY_PREV, prev ? prev.ts : null);
+  Session.set(SESSION_HISTORY_NEXT, next ? next.ts : null);
 });
 
 /**
@@ -89,7 +124,7 @@ Template.history.edits = function() {
   edits = [];
   WikiEdits.find({pageId: pageId()}, {sort: {ts: -1}}).forEach(function(edit) {
     var data, userMap;
-    userMap = Session.get(SESSION_EDIT_USER_MAP) || {};
+    userMap = Session.get(SESSION_HISTORY_USER_MAP) || {};
     data = {
       ts: edit.ts,
       pageId: pageName(),
@@ -111,7 +146,7 @@ Template.history.events({
  * @return {boolean}
  */
 Template.historicalEdit.showHistoricalEdit = function() {
-  return !_.isNull(Session.get(SESSION_EDIT_TS));
+  return !_.isNull(Session.get(SESSION_HISTORY_TS));
 };
 
 /**
@@ -119,8 +154,8 @@ Template.historicalEdit.showHistoricalEdit = function() {
  */
 Template.historicalEdit.data = function() {
   var edit, userMap;
-  userMap = Session.get(SESSION_EDIT_USER_MAP) || {};
-  edit = WikiEdits.findOne({ts: Session.get(SESSION_EDIT_TS)});
+  userMap = Session.get(SESSION_HISTORY_USER_MAP) || {};
+  edit = WikiEdits.findOne({ts: Session.get(SESSION_HISTORY_TS)});
   if (!edit) {
     return {formattedContent: 'Not found.'};
   }
@@ -137,6 +172,20 @@ Template.historicalEdit.events({
 });
 
 /**
+ * @return {number}
+ */
+Template.historicalEdit.previous = function() {
+  return Session.get(SESSION_HISTORY_PREV);
+};
+
+/**
+ * @return {number}
+ */
+Template.historicalEdit.next = function() {
+  return Session.get(SESSION_HISTORY_NEXT);
+};
+
+/**
  * @param {Object} event
  */
 function handleHistoryInternalAction(event) {
@@ -147,7 +196,9 @@ function handleHistoryInternalAction(event) {
   if (type === 'close') {
     window.router.run('history', [name], [{}, 'history', name]);
   }
-
+  if (type === 'previous' || type === 'next') {
+    handleHistoryNavEvent(event);
+  }
 };
 
 
@@ -160,10 +211,20 @@ function handleHistoryInternalLink(event) {
   type = el.attr('data-type');
   if (type === 'history') {
     event.preventDefault();
-    ts = el.attr('data-ts');
-    if (ts) {
-      name = pageName();
-      window.router.run('history', [name, ts], [{}, 'history', name, ts]);
-    }
+    handleHistoryNavEvent(event);
+  }
+}
+
+
+/**
+ * @param {Object} event
+ */
+function handleHistoryNavEvent(event) {
+  var el;
+  el = $(event.target);
+  ts = el.attr('data-ts');
+  if (ts) {
+    name = pageName();
+    window.router.run('history', [name, ts], [{}, 'history', name, ts]);
   }
 }
